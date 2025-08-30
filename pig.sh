1d62925c3133c3b0e5a3fb9e1edbb56f3bcfbef5
Nitro Jack <165733558+Nitro-Jack@users.noreply.github.com>
2025-08-30 05:34:40 +0000

    These are prototypes do not run

---
+++ pig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Variable Declaration
+CPU=$(grep "model name" /proc/cpuinfo | head -1 | awk -F: '{print $2}' | xargs)
+KERNEL=$(awk '{print $3}' /proc/version)
+OS=$(grep "PRETTY_NAME" /etc/os-release | cut -d= -f2 | tr -d '"')
+
+
+## Echoing out
+echo -e "Hi buddy, I'm inside your system.\nDon't Believe Me?"
+echo -e "Well you have a $CPU, and you're running a current version of $OS, specifically $KERNEL"
+echo -e "So you think you're hot shit??? Fuck outta here"
+
+/home/j3lly/Desktop/piggie.txt
+
+
+konsole --background-mode
